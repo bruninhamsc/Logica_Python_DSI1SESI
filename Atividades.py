bf4ae659fd0bd4a7d3d0ca9fb380b7f8ea007612
Brubs <sunnycarvalho96@gmail.com>
2026-05-13 16:27:52 -0300

    Add files via upload

---
+++ Atividades.py
@@ -0,0 +1,52 @@
+# EX1
+# Um aluno tem 10 anos. Armazene essa idade em uma variável
+# e exiba seu tipo.
+idade = 10
+print(type(idade))
+
+# EX2
+# A temperatura medida é 23.5°C.
+# Armazene esse valor e mostre seu tipo.
+temp = 23.5
+print(type(temp))
+
+# EX3
+# Crie um número complexo representando uma impedância elétrica
+# de 5 + 8j e mostre sua parte real.
+numero_complexo = 5 +8j
+print("Parte Real:", numero_complexo.real)
+
+# EX4
+# Mostre a parte imaginária do número complexo
+# criado no exercício anterior.
+
+
+# EX5
+# Declare uma variável chamada "populacao"
+# com o valor 8_000_000_000 (8 bilhões)
+# e mostre seu tipo.
+
+
+# EX6
+# Verifique se o número 7 é do tipo int
+# usando a função type().
+
+
+# EX7
+# Crie uma variável chamada "aprovado"
+# com o valor booleano True e mostre seu tipo.
+
+
+# EX8
+# Some True e False e mostre o resultado
+# e também o tipo do resultado.
+
+
+# EX9
+# Pesquise e mostre qual é o valor máximo
+# que um número inteiro pode ter em Python.
+
+
+# EX10
+# Mostre a representação em binário
+# do número 10 usando uma função do Python.
